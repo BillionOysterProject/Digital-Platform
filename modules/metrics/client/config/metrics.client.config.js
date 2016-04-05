fb19d2c1f5d497231d5b4159b0e170f8b7435896
thetif <thetif@gmail.com>
2016-04-05 10:33:34 -0600

    added initial set up for metrics

---
+++ modules/metrics/client/config/metrics.client.config.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('metrics')
+    .run(MenuConfig);
+
+  MenuConfig.$inject = ['Menus'];
+
+  function MenuConfig(Menus) {
+
+    Menus.addSubMenuItem('account', 'settings', {
+      title: 'Metrics',
+      state: 'metrics.dashboard',
+      roles: ['admin']
+    });
+  }
+})();
