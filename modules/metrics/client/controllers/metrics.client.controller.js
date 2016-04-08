ee5d5359a9175c76e8c22f701e9365ce9e5773b9
bhalt <bhalteman@fearsol.com>
2016-04-08 12:27:38 -0400

    BOP-32 comment out syntax bug

--- modules/metrics/client/controllers/metrics.client.controller.js
+++ modules/metrics/client/controllers/metrics.client.controller.js
@@ -12,7 +12,8 @@
     // TODO: can we get this wired up to put in BOP colors? (doesn't work)
     // MetricsController.setOptions({ colors : [ '#000', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'] });
     
-    Chart.defaults.global.responsive = true;
+    // TODO: says Chart is not defined 
+    //Chart.defaults.global.responsive = true;
     
     // TODO: also can this be wired up? (doesn't work)
     //Chart.defaults.global = {
