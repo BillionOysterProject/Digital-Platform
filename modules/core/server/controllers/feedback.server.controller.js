3e874f086481c613bbb78a04d25e2d74d6abef37
thetif <thetif@gmail.com>
2016-04-21 13:47:02 -0600

    user claim

---
+++ modules/core/server/controllers/feedback.server.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+var path = require('path'),
+  email = require(path.resolve('./modules/core/server/controllers/email.server.controller'));
+
+exports.bugReport = function(req, res) {
+
+};
+
+exports.general = function(req, res) {
+
+};
