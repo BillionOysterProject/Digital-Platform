0dbce5492c9e832422f37ecaaad47cd36fec0fc6
thetif <thetif@gmail.com>
2016-05-03 10:13:51 -0600

    changed allowed upload file size

--- modules/forms/server/controllers/upload-remote.server.controller.js
+++ modules/forms/server/controllers/upload-remote.server.controller.js
@@ -14,8 +14,8 @@ function UploadRemote() {
     maxAsyncS3: 20,     // this is the default
     s3RetryCount: 3,    // this is the default
     s3RetryDelay: 1000, // this is the default
-    multipartUploadThreshold: 20971520, // this is the default (20 MB)
-    multipartUploadSize: 15728640, // this is the default (15 MB)
+    multipartUploadThreshold: 30*1024*1024, // this is the default (20 MB)
+    multipartUploadSize: 25*1024*1024, // this is the default (15 MB)
     s3Options: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
