bbe23dbf190a993f6d83e1aefb51fc8c7aa5aebf
thetif <thetif@gmail.com>
2016-08-03 15:39:36 -0600

    working for create expedition; submitting protocols 1, 3, & 5 for team member1; viewing protocols 2 & 4 for team member2

--- modules/expeditions/tests/e2e/expeditions.e2e.tests.js
+++ modules/expeditions/tests/e2e/expeditions.e2e.tests.js
@@ -489,39 +489,43 @@ describe('Expedition E2E Tests', function() {
     });
   });
 
-  // describe('Team member 2 view Expedition', function() {
-  //   it ('should allow team member 2 to click protocols 2 & 4', function () {
-  //     // Sign in as team member 1
-  //     signinAs(member2);
-  //     // Assert that it went to the correct opening page
-  //     expect(browser.getCurrentUrl()).toEqual('http://localhost:8081/restoration-stations');
-  //     // Assert that there is only one expedition
-  //     var expeditions = element.all(by.repeater('expedition in vm.expeditions'));
-  //     expect(expeditions.count()).toEqual(1);
-  //     // Click on that expedition
-  //     expeditions.get(0).click();
-  //     // Assert that only protocols 1, 3, & 5 are clickable
-  //     expect(element(by.id('protocol1Link')).isDisplayed()).toBe(false);
-  //     expect(element(by.id('protocol1View')).isDisplayed()).toBe(true);
-  //     var firstLink = element(by.id('protocol2Link'));
-  //     expect(firstLink).toBe(true);
-  //     expect(element(by.id('protocol2View')).isDisplayed()).toBe(false);
-  //     expect(element(by.id('protocol3Link')).isDisplayed()).toBe(false);
-  //     expect(element(by.id('protocol3View')).isDisplayed()).toBe(true);
-  //     expect(element(by.id('protocol4Link')).isDisplayed()).toBe(true);
-  //     expect(element(by.id('protocol4View')).isDisplayed()).toBe(false);
-  //     expect(element(by.id('protocol5Link')).isDisplayed()).toBe(false);
-  //     expect(element(by.id('protocol5View')).isDisplayed()).toBe(true);
-  //     firstLink.click();
-  //   });
-  //
-  //   it ('should allow team member 2 to view protocols 2 & 4', function() {
-  //     // Oyster Measurements tab should be visible
-  //     var oysterMeasurementLink = element(by.partialLinkText('Oyster Measurements'));
-  //     expect(oysterMeasurementLink.isDisplayed()).toBe(true);
-  //     // Settlement Tiles tab should be visible
-  //     var settlementTilesLink = element(by.partialLinkText('Settlement Tiles'));
-  //     expect(settlementTilesLink.isDisplayed()).toBe(true);
-  //   });
-  // });
+  describe('Team member 2 view Expedition', function () {
+    it ('should allow team member 2 to click protocols 2 & 4', function () {
+      // Sign in as team member 2
+      signinAs(member2);
+      // Assert that it went to the correct opening page
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:8081/restoration-stations');
+      // Assert that there is only one expedition
+      var expeditions = element.all(by.repeater('expedition in vm.expeditions'));
+      expect(expeditions.count()).toEqual(1);
+      // Click on that expedition
+      expeditions.get(0).click();
+      // Assert that only protocols 2 & 4 are clickable
+      expect(element(by.id('protocol1Link')).isDisplayed()).toBe(false);
+      expect(element(by.id('protocol1View')).isDisplayed()).toBe(true);
+      var firstLink = element(by.id('protocol2Link')).isDisplayed();
+      expect(firstLink.isDisplayed()).toBe(true);
+      expect(element(by.id('protocol2View')).isDisplayed()).toBe(false);
+      expect(element(by.id('protocol3Link')).isDisplayed()).toBe(false);
+      expect(element(by.id('protocol3View')).isDisplayed()).toBe(true);
+      expect(element(by.id('protocol4Link')).isDisplayed()).toBe(true);
+      expect(element(by.id('protocol4View')).isDisplayed()).toBe(false);
+      expect(element(by.id('protocol5Link')).isDisplayed()).toBe(false);
+      expect(element(by.id('protocol5View')).isDisplayed()).toBe(true);
+      firstLink.click();
+    });
+
+    it ('should allow team member 2 to view protocols 2 & 4', function() {
+      // Oyster Measurements tab should be visible
+      expect(element(by.partialLinkText('Oyster Measurements')).isDisplayed()).toBe(true);
+      // Settlement Tiles tab should be visible
+      expect(element(by.partialLinkText('Settlement Tiles')).isDisplayed()).toBe(true);
+    });
+
+    it ('should allow team member 1 to fill out protocol 1', function() {
+      element(by.partialLinkText('Site Conditions')).click();
+      browser.sleep(1000);
+      // Fill in values
+    });
+  });
 });
