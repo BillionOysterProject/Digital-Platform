2271ec5112e21f2c2d1d3802b09781dde8df827e
Katy Ekey <katy.ekey@gmail.com>
2016-11-22 21:59:32 -0500

    BOP-1150: initialize map component with provided lat/lng
    	* Found that map components were always showing up with default lat/lng even if one had been filled in
    	* Pan the map and move the marker to the lat/lng provided

--- modules/core/client/controllers/leaflet-map.client.controller.js
+++ modules/core/client/controllers/leaflet-map.client.controller.js
@@ -40,6 +40,13 @@
       }
     };
 
+    var moveMarkerToLocation = function(location) {
+      if(mapMarker){
+        var latLng = L.latLng(location.lat, location.lng);
+        moveMarker(latLng);
+      }
+    };
+
     var zoomToLocation = function(location){
       var latlng = L.latLng(location.lat, location.lng);
       mapSelectMap.setView(latlng, settings.searchZoom, { animate: false });
@@ -72,6 +79,7 @@
       if(vm.mapControls){
         vm.mapControls.resizeMap = resizeMap;
         vm.mapControls.moveMarker = moveMarker;
+        vm.mapControls.moveMarkerToLocation = moveMarkerToLocation;
         vm.mapControls.zoomToLocation = zoomToLocation;
         vm.mapControls.zoomToLevel = zoomToLevel;
         vm.mapControls.zoomOut = zoomOut;
