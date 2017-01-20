04822ae63fd5c6e76042716d2be55e632f7a46ff
Katy Ekey <katy.ekey@gmail.com>
2017-01-19 19:15:11 -0500

    BOP-1227: metrics fixes
    	* set the chart colors and font
    	* fixes to station metrics

--- modules/metrics/client/controllers/metrics.client.controller.js
+++ modules/metrics/client/controllers/metrics.client.controller.js
@@ -5,35 +5,22 @@
     .module('metrics')
     .controller('MetricsController', MetricsController);
 
-  MetricsController.$inject = ['$scope', '$rootScope', '$timeout', 'moment'];
+  MetricsController.$inject = ['$scope', '$rootScope', '$timeout', 'moment', 'ChartJs'];
 
-  function MetricsController($scope, $rootScope, $timeout, moment) {
+  function MetricsController($scope, $rootScope, $timeout, moment, ChartJsProvider) {
 
-    // TODO: can we get this wired up to put in BOP colors? (doesn't work)
-    // MetricsController.setOptions({ colors : [ '#000', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'] });
-
-    // // TODO: says Chart is not defined
-    // Chart.defaults.global.responsive = true;
-    //
-    // // TODO: also can this be wired up? (doesn't work)
-    // Chart.defaults.global = {
-    //   animationSteps: 20, // to hopefully speed up animations
-    //   scaleFontFamily: "'Roboto'",
-    //   tooltipFontFamily: "'Roboto'",
-    //   tooltipTitleFontFamily: "'Roboto'"
-    // };
+    ChartJsProvider.Chart.defaults.global.colours = [ '#000', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'];
+    ChartJsProvider.Chart.defaults.global.responsive = true;
+    ChartJsProvider.Chart.defaults.global.animationSteps= 20;
+    ChartJsProvider.Chart.defaults.global.scaleFontFamily = 'Roboto';
+    ChartJsProvider.Chart.defaults.global.tooltipFontFamily = 'Roboto';
+    ChartJsProvider.Chart.defaults.global.tooltipTitleFontFamily = 'Roboto';
 
     var vm = this;
 
-    // vm.series = ['Team Members', 'Team Leads', 'Admin'];
-    // vm.data = [
-    //   [165, 159, 180, 181, 256, 257, 325],
-    //   [18, 18, 20, 29, 36, 43, 45],
-    //   [2, 2, 2, 2, 3, 3, 3, 3]
-    // ];
-
-    vm.onClick = function (points, evt) {
-      console.log(points, evt);
-    };
+    //not sure if we want behavior on click?
+    // vm.onClick = function (points, evt) {
+    //   console.log(points, evt);
+    // };
   }
 })();
