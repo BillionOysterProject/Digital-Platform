87516bf03ea5fce7dc4b0a4e6837b584b58909da
Gary Hetzel <garyhetzel@gmail.com>
2018-05-02 16:45:30 -0400

    Fixing indentation

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 var Raven = require('raven');
 
 switch (process.env.NODE_ENV) {
-case 'production':
-  Raven.config('https://765e170adcc642febcf1ca28f795918c@sentry.io/1200029').install();
-  break;
+  case 'production':
+    Raven.config('https://765e170adcc642febcf1ca28f795918c@sentry.io/1200029').install();
+    break;
 }
 
 var app = require('./config/lib/app');
